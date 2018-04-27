29b13ab741261def57f0e8255bc1255eb8c10a88
andry-andriamananjara <38506970+andry-andriamananjara@users.noreply.github.com>
2018-04-27 11:34:25 +0200

    Update dfnc_matrix.py

--- SZ/dfnc_matrix.py
+++ SZ/dfnc_matrix.py
@@ -1,22 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
-def window_per_people(filemane):
-	filename1 = filename
-	# Load file
-	brain_time_data = np.loadtxt(open(filename1, "rb"), delimiter=",") #Each row is a region, each column is a time point
-	
-	line_col=np.shape(brain_time_data)
+#this function cut each subject in three subparts
+
+def window_per_people(subject):
+
+	line_col=np.shape(subject)
 	line=line_col[0] #number of line
 	col=line_col[1] #number of column
 
-	shape =(96,50) #line and column 
+	shape =(96,50) #number of line and column 
 	window_one= np.zeros(shape)
 	window_two= np.zeros(shape)
 	window_three= np.zeros(shape)
 		
 	for i in range(0,line):
-		line_btd=brain_time_data[i] # we take the line 
+		line_btd=subject[i] # we take each line 
 		for j in range(0,col):
 			if (j<50 and j>=0):
 				w=line_btd[j]
@@ -31,42 +31,70 @@ def window_per_people(filemane):
 	set_window=[window_one, window_two, window_three]
 	return set_window
 
-def plot_window(obj_patient,state_name):
-	wind_with_pat= obj_patient
-	state=str(state_name)		
-	fig=plt.figure()
-	length= len(obj_patient)
-	for i in range(0,length):
-		l=wind_with_pat[i]
-		set_window=l[0]
-		patient_name=l[1]
-		for window in range(len(set_window)):
-			# Compute functional connectivity
-			corr = np.corrcoef(set_window[window]) #This is the static functional 		connectivity, to get the dynamical you have to select only few columns
-			mask =  np.tri(corr.shape[0], k=-1)
-			corr = np.ma.array(corr, mask=mask) # mask out the lower triangle
-			fig.add_subplot(length,3,window+1)
-			plt.imshow(corr, cmap='jet',origin ='upper',extent=[0., 96., 0., 96.],interpolation='nearest')
-			plt.title("Window "+str(window+1))
-		if state =='schi':
-			plt.suptitle('Patient Schizophrenia number '+str(patient_name)) 
+	
+# this function compute the correlation matrix
+
+def static_connectivity_matrix(subject):
+	corr= np.corrcoef(subject)	
+	return corr 
+	
+#flat the connectivity matrix in 1D
+def features(correlation_matrix_static):
+
+	corr= correlation_matrix_static
+	vec_1D_per_subject_static=[0]
 		
-		elif state =='control':
-			plt.suptitle('Patient Control healthy number '+str(patient_name))
+	#we take triangular superior of the correlation matrix 
+	triangular_sup = np.triu(corr,k=0)
+	
+	#transformation of the triangular matrix in 1D vector  
+	vec_1D_per_subject_static=vec_1D_per_subject_static+list(triangular_sup[np.triu_indices(96)])
+	
+	del vec_1D_per_subject_static[0]# delete the initial value 
+		
+	return vec_1D_per_subject_static
+	
+def dynamical_connectivity(win_set):
+	set_window= win_set
+	
+	dynamic_mat_list=[]
+	
+	for window in range(len(set_window)):
+		
+		# we compute the static connectivity per window which makes the dynamic		
+		corr = static_connectivity_matrix(set_window[window]) 
+			
+		# take upper part of the correlation matrix
+		triangular_sup = np.triu(corr,k=0)
+			
+		dynamic_mat_list.append(triangular_sup)
+		
+	return dynamic_mat_list
+	
 
 if __name__=="__main__":
+	sz_one_dim=[]
+	sz_dynamic_matrix=[]
+	
+	control_one_dim=[]
+	control_dynamic_matrix= []
+	
 	#data schizophrenia
-	filename_sz=["../SZ/0040001.csv","../SZ/0040007.csv","../SZ/0040015.csv","../SZ/0040037.csv","../SZ/0040080.csv","../SZ/0040089.csv","../SZ/0040099.csv","../SZ/0040108.csv","../SZ/0040034.csv","../SZ/0040105.csv","../SZ/0040122.csv","../SZ/0040096.csv","../SZ/0040132.csv","../SZ/0040100.csv","../SZ/0040142.csv","../SZ/0040145.csv"]
-	#data control healthy
-	filename_control=["0040027.csv","0040026.csv","0040043.csv","0040024.csv","0040038.csv","0040035.csv","0040033.csv","0040062.csv","0040061.csv","0040058.csv","0040056.csv","0040053.csv","0040051.csv","0040030.csv","0040063.csv","0040065.csv","0040066.csv","0040067.csv","0040068.csv"]
-	#for filename in filename_control: # for healthy control
-	for filename in filename_sz: #for schizophrenia
-		obj_patient=[]
-		win_set= window_per_people(filename)
-		obj_patient.append((win_set,filename))
-		state_name= 'schi'
-		#state_name= 'control'
-		plot_window(obj_patient,state_name)	
-	plt.colorbar()
-	plt.show()
+	filename_sz = os.listdir("SZ")
+	for filename in filename_sz: 
+		subject = np.loadtxt(open("SZ/"+filename, "rb"), delimiter=",")
+		SCM = static_connectivity_matrix(subject)
+		sz_one_dim.append(features(SCM))
+		win_set = window_per_people(subject)
+		sz_dynamic_matrix.append(dynamical_connectivity(win_set))
+	
+		
+        #data control healthy
+	filename_control = os.listdir("CONTROL")
+	for filename in filename_control:
+		subject = np.loadtxt(open("CONTROL/"+filename, "rb"), delimiter=",")
+		SCM = static_connectivity_matrix(subject)
+		control_one_dim.append(features(SCM))
+		win_set = window_per_people(subject)
+		control_dynamic_matrix.append(dynamical_connectivity(win_set))
 	
